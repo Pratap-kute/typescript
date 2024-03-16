744b83c4557983d3d4f5948c58982cbaa42d3617
Pratap Kute <pratap.kute@d10x.io>
2024-03-16 21:09:04 +0530

    studied the how to use funtions in interface

--- 02Basic/interface.ts
+++ 02Basic/interface.ts
@@ -3,5 +3,17 @@ interface User {
   email: string;
   userId: number;
   googleId?: string;
+  startTrial: () => string;
+  getCoupon(couponname: string): number;
 }
-const user: User = { dbId: 22, email: "h@h.com", userId: 1 };
+const user: User = {
+  dbId: 22,
+  email: "h@h.com",
+  userId: 1,
+  startTrial: () => {
+    return "start trial";
+  },
+  getCoupon: (couponname: "amol") => {
+    return 100;
+  },
+};

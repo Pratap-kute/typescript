f2cde2be88730394c7e137787538e5557dc610b8
Pratap Kute <pratap.kute@d10x.io>
2024-03-16 23:21:03 +0530

    first class

--- purets/src/index.ts
+++ purets/src/index.ts
@@ -1,2 +1,10 @@
-console.log("typescript is working!");
-console.log("typescript is awesome!");
+class User {
+  readonly city: string = "";
+  constructor(
+    public email: string,
+    public age: number,
+  ) {}
+}
+
+const test = new User("test@test.com", 25);
+// test.city = "New York";
